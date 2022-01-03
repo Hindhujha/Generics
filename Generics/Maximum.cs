94e23b5c35dccedbec7552c8fff08643cd40b86d
unknown <hindhujhasridhar@gmail.com>
2022-01-03 23:51:30 +0530

    MAXIMUM INTEGER VALUE IS FOUND

---
+++ Generics/Maximum.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Maximum:Numbers
+    {
+        public void ReadInput()
+        {
+            Console.WriteLine("ENTER A THREE NUMBERS");
+            Num1=Convert.ToInt32(Console.ReadLine());
+            Num2=Convert.ToInt32(Console.ReadLine());
+            Num3=Convert.ToInt32(Console.ReadLine());
+        }
+        public void Max()
+        {
+            
+           
+            List<int> num = new List<int>();
+
+           
+            num.Add(Num1);
+            num.Add(Num2);
+            num.Add(Num3);
+            Console.WriteLine("NUMBERS ARE: ");
+            foreach (int elements in num) 
+            {
+                Console.WriteLine(elements);
+            }
+            int max = num.Max();
+            Console.WriteLine("MAXIMUM NUMBER IS:" + max);
+        }
+    }
+
+    internal class Numbers
+    {
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+        public int Num3 { get; set; }
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -8,7 +8,29 @@ namespace Generics
         static void Main(string[] args)
         {
             Console.WriteLine("FIND MAXIMUM PROBLEMS USING GENERICS");
+            while (true)
+            {
+                Console.WriteLine("ENTER AN OPTION");
+                Console.WriteLine("-----------------");
+                Console.WriteLine("1.MAXIMUM VALUE OF 3 INTEGERS");
+                int select = Convert.ToInt32(Console.ReadLine());
+
+                switch (select)
+                {
+                    case 1:
+                       Maximum intmax = new Maximum();
+                        intmax.ReadInput();
+                       intmax.Max();
+                        break;
+                    default:
+                        Console.WriteLine("ENTER A CORRECT NO:");
+                        break;
+                }
+            }
 
         }
     }
+
 }
+
+              
